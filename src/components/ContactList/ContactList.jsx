53b5f8fd97865a400521e0f6d11e685476f8267f
Serhii Tarasenko <stdev33@gmail.com>
2024-03-14 18:18:15 +0200

    create selectVisibleContacts selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,35 +1,26 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContacts, deleteContact } from '../../redux/contacts/thunks';
-import {
-  selectContacts,
-  selectIsLoading,
-  selectError,
-} from '../../redux/contacts/selectors';
-import { selectFilter } from '../../redux/filter/selectors';
+import { selectIsLoading, selectError } from '../../redux/contacts/selectors';
+import { selectVisibleContacts } from '../../redux/filter/selectors';
 import ContactItem from '../ContactItem/ContactItem';
 import css from './ContactList.module.css';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
-  const filter = useSelector(selectFilter);
+  const visibleContacts = useSelector(selectVisibleContacts);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <ul className={css.contactList}>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      {filteredContacts.map(({ id, name, phone }) => (
+      {visibleContacts.map(({ id, name, phone }) => (
         <ContactItem
           key={id}
           id={id}
